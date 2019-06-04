87109b6571428ce5ce39f039d6e3f942ac412fad
Art Peel <apeel@flexport.com>
2019-06-03 17:33:27 -0700

    add helpful plugins

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.10.0"]
-                 [cheshire "5.8.1"]])
+                 [cheshire "5.8.1"]]
+
+  :plugins [[lein-cljfmt "0.6.4"]
+            [lein-ancient "0.6.15"]]) ; For detecting and fixing outdated dependencies. See https://github.com/xsc/lein-ancient
